d5e872d972190e53c63bef53582767951a62c26d
JXL <deviliveb@foxmail.com>
2017-04-01 10:21:42 +0800

    ver-0.2

--- js/base.js
+++ js/base.js
@@ -1,10 +1,15 @@
+;(function(global){
+
+var originWidth=window.ORIGIN_WIDTH||1680,
+    minWidth=window.MIN_WIDTH||800,
+    maxWidth=window.MAX_WIDTH||1680;
 onresize=onload=function(){
   function GetRoot(){return document.documentElement||document.body.parentElement;}
   function WinWidth(){return window.innerWidth||GetRoot().clientWidth;}
-  var ratio=WinWidth()/1680;
-  if(WinWidth()<=800)GetRoot().style.fontSize=800/1680*100+"px";
-  else if(WinWidth()<=1200)GetRoot().style.fontSize=ratio*100+"px";
-  else GetRoot().style.fontSize=1200/1680*100+"px";
+  var ratio=WinWidth()/originWidth;
+  if(WinWidth()<=minWidth)GetRoot().style.fontSize=minWidth/originWidth*100+"px";
+  else if(WinWidth()<=maxWidth)GetRoot().style.fontSize=ratio*100+"px";
+  else GetRoot().style.fontSize=maxWidth/originWidth*100+"px";
   GetRoot().style.opacity=1;
   if(window.LoadDeferred)LoadDeferred.resolve();
 };
@@ -15,3 +20,5 @@ onscroll=function(){
     document.getElementsByTagName("header")[0].style.backgroundColor="rgba(255,255,255,1)";
   }
 };
+
+})(window);
